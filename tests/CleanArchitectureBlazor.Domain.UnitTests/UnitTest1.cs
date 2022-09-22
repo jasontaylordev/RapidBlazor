1879b8e2f0d868e3de88c508a6779c18018c6951
Jason Taylor <jasontaylor@ssw.com.au>
2022-09-22 10:39:15 +1000

    👷 Include automated tests in build workflow

---
+++ tests/CleanArchitectureBlazor.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitectureBlazor.Domain.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO: Write some real tests.
+        }
+    }
+}
