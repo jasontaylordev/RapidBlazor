d3d2b6cf316820d8cf97eaa50d9bcbab4e9d0529
Jason Taylor <jasontaylor@ssw.com.au>
2022-09-27 20:44:43 +1000

    🧪 Port acceptance tests.

---
+++ tests/WebUI.AcceptanceTests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using CleanArchitectureBlazor.WebUI.AcceptanceTests.Pages;
+global using BoDi;
+global using FluentAssertions;
+global using Microsoft.Playwright;
+global using TechTalk.SpecFlow;
---
+++ tests/WebUI.AcceptanceTests/Pages/BasePage.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitectureBlazor.WebUI.AcceptanceTests;
+
+namespace CleanArchitectureBlazor.WebUI.AcceptanceTests.Pages;
+
+public abstract class BasePage
+{
+    public static string BaseUrl => ConfigurationHelper.GetBaseUrl();
+
+    public abstract string PagePath { get; }
+
+    public abstract IBrowser Browser { get; }
+
+    public abstract IPage Page { get; set; }
+
+    public async Task GotoAsync()
+    {
+        Page = await Browser.NewPageAsync();
+        await Page.GotoAsync(PagePath);
+    }
+}
